115704db04e5d724e158b181f788dd4cdc8b2a27
TRAINING <TRAINING@DESKTOP-K5LES96>
2023-09-20 17:07:20 +0800

    shuffle list

--- day1/src/day7/Test.java
+++ day1/src/day7/Test.java
@@ -1,21 +1,32 @@
 package day7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Test {
-
+	
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		List<String> list_Strings = new
-				ArrayList<String>(); list_Strings.add("Red");
-				list_Strings.add("Green");
-				list_Strings.add("Orange")
-				;
-				list_Strings.add("White");
-				list_Strings.add("Black");
-				System.out.println(list_Strings)
-				;
-
+		
+		System.out.println("Input things to add to an arraylist: ");
+		String things = sc.nextLine();
+		
+		List<String> list_strings = Arrays.stream(things.split(" ")).collect(Collectors.toList());
+		
+		List<Integer>nums = new ArrayList<>();
+		
+		for(int i = 0; i<list_strings.size();i++)
+			nums.add(Integer.parseInt(list_strings.get(i)));
+		
+		System.out.println(list_strings);
+		Collections.shuffle(list_strings);
+		System.out.println(list_strings);
+		
+		
 
 	}
 
